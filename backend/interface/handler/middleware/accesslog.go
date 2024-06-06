51dbe04bec23ed4bc3c0ae4b513c906d9efe0efd
Shintaro Kawabata <k.s.espo.bue@gmail.com>
2024-06-06 09:48:15 +0900

    Sample metrics

--- backend/interface/handler/middleware/accesslog.go
+++ backend/interface/handler/middleware/accesslog.go
@@ -1,9 +1,13 @@
 package middleware
 
 import (
+	"log"
 	"log/slog"
 	"net/http"
 	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/metric"
 )
 
 type loggingWriter struct {
@@ -23,12 +27,31 @@ func (lw *loggingWriter) WriteHeader(code int) {
 	lw.ResponseWriter.WriteHeader(code)
 }
 
+var (
+	meter      = otel.Meter("fileuploader-api-meter")
+	requestCnt metric.Int64Counter
+)
+
+func init() {
+	var err error
+	requestCnt, err = meter.Int64Counter(
+		"request",
+		metric.WithDescription("Counts of Request for API"),
+		metric.WithUnit("count"))
+	if err != nil {
+		panic(err)
+	}
+}
+
 func UseAccessLogging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		start := time.Now()
 		rlw := newLoggingWriter(w)
 
 		next.ServeHTTP(rlw, req)
+
+		requestCnt.Add(req.Context(), 1)
+		log.Printf("Doing really hard work requestCnt\n", 1)
 
 		slog.InfoContext(req.Context(), "access log",
 			slog.String("URI", req.RequestURI),
